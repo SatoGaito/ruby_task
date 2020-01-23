f308effc0a807f64881b5751da05852642ea9350
sato gaito <gaito4570@gmail.com>
2020-01-23 21:00:31 +0900

    [Modified] Q17,18 [Finish] Q19,20

--- task.rb
+++ task.rb
@@ -170,7 +170,7 @@ end
 
 class UserQ17
   # 以下に回答を記載
-  def initialize(info)
+  def initialize(**info)
     @name = info[:name]
     @age = info[:age]
     @gender = info[:gender]
@@ -178,10 +178,13 @@ class UserQ17
   end
 
   def info
-    puts "名前 : #{@name}"
-    puts "年齢 : #{@age}"
-    puts "性別 : #{@gender}"
-    puts "管理者権限 : #{@admin == true ? "有り" : "無し"}"
+    puts <<~EOS
+      名前 : #{@name}
+      年齢 : #{@age}
+      性別 : #{@gender}
+      管理者権限 : #{@admin == true ? "有り" : "無し"}
+    EOS
+
   end
 
 
@@ -207,10 +210,10 @@ class UserQ18
 
   def introduce
     case @age
-    when 32
-      puts "こんにちは，あじーと申します。宜しくお願いいたします。"
-    when 10
-      puts "はいさいまいど〜，ゆたぼんです！！！"
+    when 15..35
+      print "こんにちは，あじーと申します。宜しくお願いいたします。"
+    when 0..10
+      print "はいさいまいど〜，ゆたぼんです！！！"
     end
     
   end
@@ -229,8 +232,12 @@ end
 class Item
   # 以下を修正して下さい
 
-  def initialize(name)
-    @name = name
+  def initialize(**name)
+    @name = name[:name]
+  end
+
+  def name 
+    puts "#{@name}"
   end
 
 end
@@ -243,11 +250,36 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  def initialize(**info)
+    @name = info[:name]
+    @age = info[:age]
+  end
+  attr_reader :name, :age
 end
 
 class Zoo
   # 以下に回答を記載
+
+  def initialize(**info)
+    @name = info[:name]
+    @infant = info[:entry_fee][:infant]
+    @children = info[:entry_fee][:children]
+    @adult = info[:entry_fee][:adult]
+    @senior = info[:entry_fee][:senior]
+  end
+
+  def info_entry_fee(user)
+    case user.age
+    when 0..5
+      puts "#{user.name}の入場料金は #{@infant}円です。"
+    when 6..12
+      puts "#{user.name}の入場料金は #{@children}円です。"
+    when 13..64
+      puts "#{user.name}の入場料金は #{@adult}円です。"
+    when 65..120
+      puts "#{user.name}の入場料金は #{@senior}円です。"
+    end
+  end
 
 end
 
